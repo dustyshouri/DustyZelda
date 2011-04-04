6f3a48fa250c3f33fbc731768ee18fb376482529
Dusty <dustyshouri@hotmail.com>
2011-04-03 22:45:59 -0400

    Fixed tabs.

--- src/myfirstzelda/Camera.java
+++ src/myfirstzelda/Camera.java
@@ -4,23 +4,23 @@ public class Camera {
   float x,y;
   int lvlw,lvlh;
   
-	public Camera(float nx,float ny) {
-		this.x = nx;
-		this.y = ny;
-	}
-	
-	public void getLevelData(int w,int h) {
-		lvlw = w;
-		lvlh = h;
-	}
-	
-	public void follow(float px,float py,float cx,float cy) {
-		this.x = px - cx/2 + 1;
-		this.y = py - cy/2 + 1;
-		
-		if (this.x < 0) this.x = 0;
-		if (this.y < 0) this.y = 0;
-  	if (this.x > lvlw - cx) this.x = lvlw - cx;
-  	if (this.y > lvlh - cy) this.y = lvlh - cy;
-	}
+  public Camera(float nx,float ny) {
+    this.x = nx;
+    this.y = ny;
+  }
+  
+  public void getLevelData(int w,int h) {
+    lvlw = w;
+    lvlh = h;
+  }
+  
+  public void follow(float px,float py,float cx,float cy) {
+    this.x = px - cx/2 + 1;
+    this.y = py - cy/2 + 1;
+    
+    if (this.x < 0) this.x = 0;
+    if (this.y < 0) this.y = 0;
+    if (this.x > lvlw - cx) this.x = lvlw - cx;
+    if (this.y > lvlh - cy) this.y = lvlh - cy;
+  }
 }
